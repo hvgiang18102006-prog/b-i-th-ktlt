78c8aa53f2041d1735396671f797284d9bb92114
hvgiang18102006-prog <hvgiang18102006@gmail.com>
2025-12-11 07:38:40 +0700

    Add files via upload

---
+++ bài_6/bai6_5.py
@@ -0,0 +1,12 @@
+# Hoàng Văn Giang, mssv 245752021610114
+
+class WordReverser:
+    def __init__(self, text):
+        self.text = text
+
+    def reverse_words(self):
+        words = self.text.split()
+        reversed_words = words[::-1]
+        return ' '.join(reversed_words)
+s = WordReverser("hello .py")
+print(s.reverse_words())  # Kết quả: .py hello
